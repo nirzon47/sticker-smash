35d02ba3b1de2210a44e0639626f676f565a19ea
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2024-05-25 00:26:58 +0530

    feat: button

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, View } from 'react-native'
 import ImageViewer from './ImageViewer'
+import Button from './Button'
 
 export default function App() {
 	return (
 		<View style={styles.container}>
 			<ImageViewer />
+			<View style={styles.footerContainer}>
+				<Button label='Choose a photo' variant='primary' />
+				<Button label='Use this photo' />
+			</View>
 			<StatusBar style='auto' />
 		</View>
 	)
@@ -15,6 +20,10 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#25292e',
+		alignItems: 'center',
+	},
+	footerContainer: {
+		flex: 1 / 3,
 		alignItems: 'center',
 	},
 })
